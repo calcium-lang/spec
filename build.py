fd3f4c1ff105e79b8d397d620fbb549a29b9b3cf
natanjunges <natanajunges@gmail.com>
2021-10-12 21:09:26 -0300

    Add type hints

--- build.py
+++ build.py
@@ -19,13 +19,14 @@
 
 from grammar2md.alchemist.grammar2md import Syntax
 import re
+from typing import TextIO
 
-def main():
-    input = open("Syntax.grammar")
-    md = Syntax.generate(input.read(), {"Identifier", "ConstantExpression", "NumberLiteral", "BlockStatement"}, "Syntax")
+def main() -> None:
+    input: TextIO = open("Syntax.grammar")
+    md: str = Syntax.generate(input.read(), {"Identifier", "ConstantExpression", "NumberLiteral", "BlockStatement"}, "Syntax")
     input.close()
     md = re.sub(r"## ([a-zA-Z]+):", r"## [\1](Semantics.md#\1):", md)
-    output = open("Syntax.md", "w")
+    output: TextIO = open("Syntax.md", "w")
     output.write(md)
     output.close()
 
