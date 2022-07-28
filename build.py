6f4ef829e4b9ccc9905a3228f7e75ea409723ac8
Natan Junges <natanajunges@gmail.com>
2022-07-28 18:44:41 +0000

    Update build.py

--- build.py
+++ build.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 
-# This script is part of Cesium.
+# This is free and unencumbered software released into the public domain.
 #
-# Copyright (C) 2021-2022  Natan Junges <natanajunges@gmail.com>
-#
-# This script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <https://www.gnu.org/licenses/>.
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
 
 from grammar2md.alchemist.grammar2md import Syntax
 
